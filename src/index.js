faad640f4f6febe0aa1641509588ef219005002a
Arthur <nesterenko19@gmail.com>
2018-02-06 22:28:28 +0200

    add test

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 import styles from './main.scss';
 
-const App = () => <div className={styles.app}>Hello react skeleton</div>;
+export const App = () => <div className={styles.app}>Hello react skeleton</div>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
