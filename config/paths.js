5805a71ab3b2912c8bd1f5a5a95670d1f51ba8f7
Arthur <nesterenko19@gmail.com>
2018-02-06 22:10:02 +0200

    run prettier

--- config/paths.js
+++ config/paths.js
@@ -10,39 +10,38 @@ const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 const envPublicUrl = process.env.PUBLIC_URL;
 
 function ensureSlash(path, needsSlash) {
-    const hasSlash = path.endsWith('/');
-    if (hasSlash && !needsSlash) {
-        return path.substr(path, path.length - 1);
-    } else if (!hasSlash && needsSlash) {
-        return `${path}/`;
-    } else {
-        return path;
-    }
+  const hasSlash = path.endsWith('/');
+  if (hasSlash && !needsSlash) {
+    return path.substr(path, path.length - 1);
+  } else if (!hasSlash && needsSlash) {
+    return `${path}/`;
+  } else {
+    return path;
+  }
 }
 
 const getPublicUrl = appPackageJson =>
-    envPublicUrl || require(appPackageJson).homepage;
-
+  envPublicUrl || require(appPackageJson).homepage;
 
 function getServedPath(appPackageJson) {
-    const publicUrl = getPublicUrl(appPackageJson);
-    const servedUrl =
-        envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/');
-    return ensureSlash(servedUrl, true);
+  const publicUrl = getPublicUrl(appPackageJson);
+  const servedUrl =
+    envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/');
+  return ensureSlash(servedUrl, true);
 }
 
 // config after eject: we're in ./config/
 module.exports = {
-    dotenv: resolveApp('.env'),
-    appBuild: resolveApp('build'),
-    appPublic: resolveApp('public'),
-    appHtml: resolveApp('public/index.html'),
-    appIndexJs: resolveApp('src/index.js'),
-    appPackageJson: resolveApp('package.json'),
-    appSrc: resolveApp('src'),
-    yarnLockFile: resolveApp('yarn.lock'),
-    testsSetup: resolveApp('src/setupTests.js'),
-    appNodeModules: resolveApp('node_modules'),
-    publicUrl: getPublicUrl(resolveApp('package.json')),
-    servedPath: getServedPath(resolveApp('package.json')),
+  dotenv: resolveApp('.env'),
+  appBuild: resolveApp('build'),
+  appPublic: resolveApp('public'),
+  appHtml: resolveApp('public/index.html'),
+  appIndexJs: resolveApp('src/index.js'),
+  appPackageJson: resolveApp('package.json'),
+  appSrc: resolveApp('src'),
+  yarnLockFile: resolveApp('yarn.lock'),
+  testsSetup: resolveApp('src/setupTests.js'),
+  appNodeModules: resolveApp('node_modules'),
+  publicUrl: getPublicUrl(resolveApp('package.json')),
+  servedPath: getServedPath(resolveApp('package.json')),
 };
--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -9,30 +9,30 @@ const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
 const host = process.env.HOST || '0.0.0.0';
 
 module.exports = function(proxy, allowedHost) {
-    return {
-        disableHostCheck:
-        !proxy || process.env.DANGEROUSLY_DISABLE_HOST_CHECK === 'true',
-        compress: true,
-        clientLogLevel: 'none',
-        contentBase: paths.appPublic,
-        watchContentBase: true,
-        hot: true,
-        publicPath: config.output.publicPath,
-        quiet: true,
-        watchOptions: {
-            ignored: /node_modules/,
-        },
-        https: protocol === 'https',
-        host: host,
-        overlay: false,
-        historyApiFallback: {
-            disableDotRule: true,
-        },
-        public: allowedHost,
-        proxy,
-        setup(app) {
-            app.use(errorOverlayMiddleware());
-            app.use(noopServiceWorkerMiddleware());
-        },
-    };
+  return {
+    disableHostCheck:
+      !proxy || process.env.DANGEROUSLY_DISABLE_HOST_CHECK === 'true',
+    compress: true,
+    clientLogLevel: 'none',
+    contentBase: paths.appPublic,
+    watchContentBase: true,
+    hot: true,
+    publicPath: config.output.publicPath,
+    quiet: true,
+    watchOptions: {
+      ignored: /node_modules/,
+    },
+    https: protocol === 'https',
+    host: host,
+    overlay: false,
+    historyApiFallback: {
+      disableDotRule: true,
+    },
+    public: allowedHost,
+    proxy,
+    setup(app) {
+      app.use(errorOverlayMiddleware());
+      app.use(noopServiceWorkerMiddleware());
+    },
+  };
 };
